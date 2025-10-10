303e16c4417e40dfe4e84ab7cc3ad8de3d3dfd8b
Ziad Malik <zmalik4@my.bcit.ca>
2025-10-09 19:44:18 -0700

    I have created the HTML skeleton of the website, it has a title, nav-bar, buttons, an information block in the middle and some images with testimonials

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
 
-function sayHello() {
-    
-}
-// document.addEventListener('DOMContentLoaded', sayHello);
+function sayHello() {}
+// basic mobile menu
+const btn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav");
+btn.addEventListener("click", () => {
+  nav.classList.toggle("open");
+  const open = nav.classList.contains("open");
+  btn.setAttribute("aria-expanded", open ? "true" : "false");
+});
+// year
+document.getElementById("y").textContent = new Date().getFullYear();
+document.addEventListener("DOMContentLoaded", sayHello);
