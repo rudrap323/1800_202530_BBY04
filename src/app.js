e9417cd01d4b0b826cdd0bb7609e121906103645
Ziad Malik <zmalik4@my.bcit.ca>
2025-10-27 23:58:16 -0700

    Added firebase auth, login page, main page

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+//--------------------------------------------------------------
+// If you have custom global styles, import them as well:
+//--------------------------------------------------------------
+import "/src/styles/style.css";
+
+//--------------------------------------------------------------
+// Custom global JS code (shared with all pages)can go here.
+//--------------------------------------------------------------
